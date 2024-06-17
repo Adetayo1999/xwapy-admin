f03c43cf1147b3f07cdbebb33c4fe2f4216f57ff
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2024-06-17 12:13:04 +0100

    chore: fixed 404 error issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { Route, Routes, useLocation } from "react-router-dom";
-import { generateModalRoute, generateRoute, routes } from "./common/routes";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import {
+  generateModalRoute,
+  generateRoute,
+  paths,
+  routes,
+} from "./common/routes";
 import { modalRoutes } from "./common/routes/modal-route";
 import Error404 from "./common/components/404-page";
+import { fetchUserToken } from "./common/services/storage";
 
 function App() {
   const location = useLocation();
@@ -11,7 +17,16 @@ function App() {
     <>
       <Routes location={previousLocation || location}>
         {routes.map((route) => generateRoute(route))}
-        <Route element={<Error404 />} path="*" />
+        <Route
+          element={
+            !fetchUserToken() ? (
+              <Navigate to={paths.auth.login} />
+            ) : (
+              <Error404 />
+            )
+          }
+          path="*"
+        />
       </Routes>
       {previousLocation ? (
         <Routes>{modalRoutes.map((route) => generateModalRoute(route))}</Routes>
